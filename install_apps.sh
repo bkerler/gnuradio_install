057c0c8bdaa1539ead034c7fe7effef099055d69
Bjoern Kerler <info@revskills.de>
2023-03-05 17:03:54 +0100

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -29,6 +29,7 @@ git clone https://github.com/handiko/gr-APRS
 git clone https://github.com/handiko/gr-HDLC-AFSK
 git clone https://github.com/argilo/sdr-examples
 git clone https://github.com/jhonnybonny/CleverJAM -b maint-3.10
+git clone https://github.com/RUB-SysSec/DroneSecurity
 
 echo "Installing apps"
 sudo apt install fldigi qsstv inspectrum -y
